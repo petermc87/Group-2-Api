e079102f6ce83479624ae3a7167d2f9aa9fe8fd0
Josue <cuellarjosue3@gmail.com>
2022-11-15 18:17:21 -0800

    started the configurations

--- server.js
+++ server.js
@@ -8,6 +8,19 @@ const MongoStore = require('connect-mongo')
 const db = require('./models/db')
 const app = express()
 
+// Start Config //
+// This code makes us have req.body <=====
+app.use(express.urlencoded({ extended: true }))
+app.use((req, res, next) => {
+    res.locals.data = {}
+    next()
+})
+app.use(cors())
+app.engine('jsx', require('jsx-view-engine').createEngine())
+app.set('view engine', 'jsx') // register the jsx view engine
+db.once('open', () => {
+    console.log('connected to MongoDB Atlas')
+})
 
 // Tell the app to listen on port
 app.listen(3000, () => {
